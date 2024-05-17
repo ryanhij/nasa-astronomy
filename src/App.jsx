161d203a5d2bd2925fd8a1f0f7749c62b0e03ab2
ryanhij <ksantiagoatreyu@gmail.com>
2024-05-17 12:19:10 -0600

    API data is now being displayed and the application is fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import SideBar from "./components/SideBar"
 import Footer from "./components/Footer"
 
 function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
   const [showModal, setShowModal] = useState(false)
   
   const handleToggleModal = () => {
@@ -16,10 +18,22 @@ function App() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
       const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
 
+      const today = (new Date()).toDateString()
+      const localKey = `NASA-${today}`
+
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData)
+        console.log('Fetched from cache today')
+        return
+      }
+      localStorage.clear()
 
       try {
         const res = await fetch(url)
         const apiData = await res.json()
+        localStorage.setItem(localKey, JSON.stringify(apiData))
+        setData(apiData)
         console.log(apiData)
         console.log('Fetched from API today')
       } catch (err) {
@@ -31,11 +45,15 @@ function App() {
 
   return (
     <>
-      <Main />
+      {data ? (<Main data={data} />) : 
+        <div className="loadingState">
+          <i className="fa-solid fa-gear"></i>
+        </div>
+      }
       {showModal && (
-        <SideBar handleToggleModal={handleToggleModal} />
+        <SideBar data={data} handleToggleModal={handleToggleModal} />
       )}
-      <Footer handleToggleModal={handleToggleModal} />
+      {data && (<Footer data={data} handleToggleModal={handleToggleModal} />)}
     </>
   )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 export default function SideBar(props) {
-  const {handleToggleModal} = props
+  const {handleToggleModal, data} = props
 
 
   return (
     <div className="sidebar">
         <div onClick={handleToggleModal} className="bgOverlay"></div>
         <div className="sidebarContents">
-            <h2>The Brutal Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p>ajlfs;jda;ljfdladsjflsafjl;ajl</p>
+            <h2>{data?.title}</h2>
+            <div className="descriptionContainer">
+                <p className="descriptionTitle">
+                    {data?.date}
+                </p>
+                <p>{data?.explanation}</p>
             </div>
             <button onClick={handleToggleModal}>
                 <i className="fa-solid fa-arrow-right"></i>
